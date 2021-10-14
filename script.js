5014d8c04e2790f4140a103532cba3a2fd91caf2
Chylox <chylox_bwin@protonmail.com>
2021-10-14 21:00:25 +0200

    First Sketcher, 16x16, Green, NonModular

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
+// Defining SELECTORS:
 const gridContainer = document.querySelector('#gridContainer');
 
+// Defining VARIABLES:
 
-gridContainer.appendChild(myDiv);
+
+
+
+// Generate <divs> for grid:
+for (let i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+
+    div.setAttribute('class', 'gridDivs')
+    div.setAttribute('id', `div${i+1}`);
+
+    div.style.cssText = "border: solid 1px black; width: 60px; height: 60px;";
+
+    gridContainer.appendChild(div);
+}
+
+
+
+
+
+// Eventhandler:
+const gridElements = document.querySelectorAll('.gridDivs');
+
+gridElements.forEach((element) => {
+    element.addEventListener('mouseover', function(e) {
+        element.setAttribute('style', 'background-color: green; border: solid 1px black; width: 60px; height: 60px;')
+    })
+});
